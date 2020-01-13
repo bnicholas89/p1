525b59cd51fa2f354d066427777e4c45fb41f919
Nicholas Bates <nbates66@gmail.com>
2020-01-12 19:59:32 -0800

    update floor choice and item append

--- Elevator.py
+++ Elevator.py
@@ -20,17 +20,22 @@ while True:
         print_pause("After a few moments, you find "
                     "yourself in the lobby.")
         if "ID card" in items:
-            print_sleep("The clerk greets you, but she has already "
+            print_pause("The clerk greets you, but she has already "
                         "given you your ID card, so there is nothing "
                         "more to do here now.")
         else:
-            print_sleep("The clerk greets you and gives you your ID "
+            print_pause("The clerk greets you and gives you your ID "
                         "card.")
             items.append("ID card")
     elif floor == '2':
         print_pause("You push the button for the second floor.")
         print_pause("After a few moments, you find yourself "
                     "in the human resources department.")
+        if "Handbook" in items:
+            print_pause("The HR folks are busy at their desks. "
+                        "There doesn't seem to be much to do here.")
+        else:
+            print_pause("The head of HR greets you.")
     elif floor == '3':
         print_pause("You push the button for the third floor.")
         print_pause("After a few moments, you find yourself "
